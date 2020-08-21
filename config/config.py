b7a9d8575fdd90dcf9925ce3d21ec18584cd6079
Sherzodbek <sherzodbek@vps-5841.novalocal>
2020-08-21 02:14:31 +0000

    add config folder

---
+++ config/config.py
@@ -0,0 +1,9 @@
+""" CARDEL SHIPPER BOT TOKEN """
+TOKEN = 'BOT TOKEN'
+
+mysql_host_config = {
+    'host': 'localhost',
+    'user': 'username',
+    'passwd': 'db password',
+    'database': 'testdb'
+}
