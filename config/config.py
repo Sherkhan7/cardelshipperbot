6c1691bcc77b0326176d1ec4eb68577e46935b68
Sherzodbek <sherzodbek@vps-5841.novalocal>
2020-08-21 03:52:18 +0000

    rm  cache test

--- config/config.py
+++
@@ -1,9 +0,0 @@
-""" CARDEL SHIPPER BOT TOKEN """
-TOKEN = 'bot token'
-
-mysql_host_config = {
-    'host': 'localhost',
-    'user': 'username',
-    'passwd': 'passwd',
-    'database': 'testdb'
-}
