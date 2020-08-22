623c5cac98478b6e2eed1ff822837046642c695c
sherkhan7 <9669470@gmail.com>
2020-08-22 20:18:18 +0500

    some change

--- contact.py
+++ contact.py
@@ -16,12 +16,12 @@ def contact_callback(update: Update, context: CallbackContext):
     if user:
         if user_data['LANG'] == 'uzbek':
             context.bot.edit_message_text("Kontaktingiz ma'lumotlar bazasidan topildi",
-                                          update.effective_chat.id, update.effective_message.id)
+                                          update.effective_chat.id, update.effective_message.message_id)
             # update.edited_message_text("Kontaktingiz ma'lumotlar bazasidan topildi")
             # update.message.reply_text("Kontaktingiz ma'lumotlar bazasidan topildi", reply_markup=ReplyKeyboardRemove())
         else:
             context.bot.edit_message_text("Ваш контакт был найден в базе",
-                                          update.effective_chat.id, update.effective_message.id)
+                                          update.effective_chat.id, update.effective_message.message_id)
 
             # update.edited_message("Ваш контакт был найден в базе")
             # update.message.reply_text('Ваш контакт был найден в базе', reply_markup=ReplyKeyboardRemove())
