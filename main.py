20ecc25d80424734119d4b5b2bba4aee27116d0b
sherkhan7 <9669470@gmail.com>
2020-09-07 22:27:52 +0500

    delete some functions

--- main.py
+++ main.py
@@ -7,25 +7,6 @@ from hadlers import (message_handler, inline_keyboard_handler, conversation_hand
                      changedataconversation_handler, new_cargo_conversation_handler)
 
 
-def do_new(update, context):
-    print('Inside do_new')
-
-
-def do_new2(update, context):
-    print('Inside do_new2')
-
-
-def handler_1_callback(update, context):
-    query = update.callback_query
-    data = query.data
-    print('Handled 1!')
-    print(data)
-
-
-def handler_2_callback(update, context):
-    print('Handled 2')
-
-
 def main():
     updater = Updater(TOKEN, use_context=True)
 
