9a22fdb2d3356b74395d778eec36098e6f08feb7
sherkhan7 <9669470@gmail.com>
2020-10-25 21:55:02 +0500

    delete hand made user dict in helperfunctions.py

--- helpers/helperfuctions.py
+++ helpers/helperfuctions.py
@@ -7,9 +7,11 @@ def set_user_data_in_bot_data(user_id, bot_data):
         user = get_user(user_id)
 
         if user:
-            bot_data[user_id] = {'id': user['id'], 'user_id': user['user_id'], 'name': user['name'],
-                                 'surname': user['surname'], 'first_name': user['first_name'],
-                                 'phone_number': user['phone_number'], 'lang': user['lang'],
-                                 'phone_number2': user['phone_number2']}
+            user.pop('created_at')
+            user.pop('updated_at')
+
+            bot_data[user_id] = user
+
         else:
+
             bot_data[user_id] = False
