096402e05c460b38decdcfef14ef1f019f3a5797
sherkhan7 <9669470@gmail.com>
2020-11-11 10:17:54 +0500

    major changes in conversation.py

--- handlers/conversation.py
+++ handlers/conversation.py
@@ -1,7 +1,6 @@
 from telegram import Update
-from telegram.ext import (CommandHandler, MessageHandler, ConversationHandler,
-                          CallbackContext, Filters, CallbackQueryHandler)
-from telegram import ReplyKeyboardRemove
+from telegram.ext import (CommandHandler, MessageHandler, ConversationHandler, CallbackContext, Filters,
+                          CallbackQueryHandler)
 from inlinekeyboards import InlineKeyboard
 from filters import *
 from replykeyboards import ReplyKeyboard
@@ -9,15 +8,11 @@ from DB import insert_user
 from helpers import set_user_data_in_bot_data
 from languages import LANGS
 import logging
-import random
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
 logger = logging.getLogger()
 
-SPECIAL_CODE = {}
-
-USER_ID, FIRST_NAME, LAST_NAME, FULL_NAME, LANG, PHONE_NUMBER, CODE, WHO_IS = \
-    ('user_id', 'first_name', 'last_name', 'full_name', 'lang', 'phone_number', 'code', 'who_is')
+USER_ID, USERNAME, FULLNAME, LANG = ('user_id', 'username', 'fullname', 'lang')
 
 
 def do_command(update: Update, context: CallbackContext):
@@ -34,19 +29,29 @@ def do_command(update: Update, context: CallbackContext):
 
     # logger.info('user_input_data: %s', user_input_data)
 
-    if command == '/start':
+    if command == '/start' or command == '/menu':
 
         if user:
 
             if user['lang'] == LANGS[0]:
-                text = 'Siz ro\'yxatdan o\'tgansiz !'
+                text = 'Siz ro\'yxatdan o\'tgansiz'
 
             if user['lang'] == LANGS[1]:
-                text = 'Вы зарегистрированы !'
+                text = 'Вы зарегистрированы'
 
-            text = '\U000026A0 ' + text
+            text = f'\U000026A0 {text} !'
+
+            if command == '/menu':
+
+                if user['lang'] == LANGS[0]:
+                    reply_text = 'Menu'
+
+                if user['lang'] == LANGS[1]:
+                    reply_text = 'Меню'
+
+                text = f'\U0001F4D6 {reply_text}'
+
             reply_keyboard = ReplyKeyboard('menu_keyboard', user['lang']).get_keyboard()
-
             update.message.reply_text(text, reply_markup=reply_keyboard)
 
             return ConversationHandler.END
@@ -54,8 +59,7 @@ def do_command(update: Update, context: CallbackContext):
         else:
 
             user_input_data[USER_ID] = update.effective_user.id
-            user_input_data[FIRST_NAME] = update.effective_user.first_name
-            user_input_data[LAST_NAME] = update.effective_user.last_name
+            user_input_data[USERNAME] = update.effective_user.username
 
             inline_keyboard = InlineKeyboard('langs_keyboard').get_keyboard()
             update.message.reply_text('Tilni tanlang. Выберите язык.', reply_markup=inline_keyboard)
@@ -66,159 +70,80 @@ def do_command(update: Update, context: CallbackContext):
 def lang_callback(update: Update, context: CallbackContext):
     # with open('../update.json', 'w') as update_file:
     #     update_file.write(update.to_json())
-    #
     # with open('../callback_query.json', 'w') as callback_query_file:
     #     callback_query_file.write(callback_query.to_json())
     callback_query = update.callback_query
-    data = callback_query.data
 
-    user_input_data = context.user_data
-    user_input_data[LANG] = data
+    if callback_query:
 
-    logger.info('user_input_data: %s', user_input_data)
+        data = callback_query.data
 
-    if data == LANGS[0]:
-        edit_text = 'Til: \U0001F1FA\U0001F1FF'
-        text = 'Salom !\n' \
-               'Ism va familyangizni yuboring:\n\n' \
-               '<b><i><u>Misol: Sherzod Esanov</u></i></b>'
+        user_input_data = context.user_data
+        user_input_data[LANG] = data
 
-    if data == LANGS[1]:
-        edit_text = 'Язык: \U0001F1F7\U0001F1FA'
-        text = 'Привет !\n' \
-               'Отправьте свое имя и фамилию:\n\n' \
-               '<b><i><u>Пример: Шерзод Эсанов</u></i></b>'
+        logger.info('user_input_data: %s', user_input_data)
 
-    text = '\U0001F44B ' + text
+        if data == LANGS[0]:
+            edit_text = 'Til: \U0001F1FA\U0001F1FF'
+            example = 'Misol: Sherzod Esanov'
+            text = 'Salom !\n' \
+                   'Ism va familyangizni quyidagi formatda yuboring'
 
-    callback_query.edit_message_text(edit_text)
-    callback_query.message.reply_html(text)
+        if data == LANGS[1]:
+            edit_text = 'Язык: \U0001F1F7\U0001F1FA'
+            example = 'Пример: Шерзод Эсанов'
+            text = 'Привет !\n' \
+                   'Отправьте свое имя и фамилию в формате ниже'
 
-    return FULL_NAME
+        example = f'<b><i><u>{example}</u></i></b>'
+        text = f'\U0001F44B {text}:\n\n {example}'
+
+        callback_query.edit_message_text(edit_text)
+        callback_query.message.reply_html(text)
+
+        return FULLNAME
+
+    else:
+
+        update.message.reply_text('Tilni tanlang. Выберите язык.', quote=True)
+
+        return LANG
 
 
-def full_name_callback(update: Update, context: CallbackContext):
+def fullname_callback(update: Update, context: CallbackContext):
     # print('full_name_callback')
     # with open('../update.json', 'w') as update_file:
     #     update_file.write(update.to_json())
     text = update.message.text
-    user_input_data = context.user_data
-
-    logger.info('user_input_data: %s', user_input_data)
-
-    full_name = full_name_filter(text)
-
-    if full_name:
-
-        user_input_data['name'] = full_name[0]
-        user_input_data['surname'] = full_name[1]
-
-        if user_input_data[LANG] == LANGS[0]:
-            text = 'Davom etish uchun kontaktingizni yuboring:'
-
-        if user_input_data[LANG] == LANGS[1]:
-            text = 'Чтобы продолжить, отправьте свой контакт:'
-
-        reply_keyboard = ReplyKeyboard('phone_number_keyboard', user_input_data[LANG]).get_keyboard()
-
-        update.message.reply_text(text, reply_markup=reply_keyboard)
-
-        return PHONE_NUMBER
-
-    else:
-
-        if user_input_data[LANG] == LANGS[0]:
-            text = 'Ism va familya xato yuborildi !\n' \
-                   'Qaytada quyidagi shaklda yuboring:\n\n' \
-                   '<b><i><u>Misol: Sherzod Esanov</u></i></b>'
-
-        if user_input_data[LANG] == LANGS[1]:
-            text = 'Имя и фамилия введено неверное !\n' \
-                   'Повторно введите имя и фамилия в следующем виде:\n\n' \
-                   '<b><i><u>Пример: Шерзод Эсанов</u></i></b>'
-
-        text = '\U000026A0 ' + text
-        update.message.reply_html(text, quote=True)
-
-        return FULL_NAME
-
-
-def phone_number_callback(update: Update, context: CallbackContext):
-    # with open('update.json', 'w') as update_file:
-    #     update_file.write(update.to_json())
-    global SPECIAL_CODE
-    user_input_data = context.user_data
-
-    if update.message.contact:
-
-        phone_number = update.effective_message.contact.phone_number
-
-        if not phone_number.startswith('+'):
-            phone_number = phone_number.rjust(13, '+')
-
-    else:
-
-        if user_input_data[LANG] == LANGS[0]:
-            text = 'Kontaktingizni yuboring'
-
-        if user_input_data[LANG] == LANGS[1]:
-            text = 'Отправьте свой контакт'
-
-        text = f'\U000026A0 {text} !'
-        update.message.reply_text(text, quote=True)
-
-        return PHONE_NUMBER
-
-    user_input_data[PHONE_NUMBER] = phone_number
-    logger.info('user_input_data: %s', user_input_data)
-
-    special_code = random.randint(100000, 999999)
-
-    SPECIAL_CODE[update.effective_user.id] = special_code
-
-    if user_input_data[LANG] == LANGS[0]:
-        text = f'Maxsus kod: <pre><i>{special_code}</i></pre>\n' \
-               f'Ushbu kodni menga yuboring.'
-
-    if user_input_data[LANG] == LANGS[1]:
-        text = f'Специальный код: <pre><i>{special_code}</i></pre>\n' \
-               f'Отправьте мне этот код.'
-
-    update.message.reply_html(text, reply_markup=ReplyKeyboardRemove())
-
-    return CODE
-
-
-def special_code_callback(update: Update, context: CallbackContext):
-    # with open('../update.json', 'w') as update_file:
-    #     update_file.write(update.to_json())
-    special_code = update.effective_message.text
 
     user_input_data = context.user_data
     bot_data = context.bot_data
 
-    user_input_data[CODE] = special_code
     logger.info('user_input_data: %s', user_input_data)
-    logger.info('SPECIAL_CODE: %s', SPECIAL_CODE)
 
-    special_code = special_code_filter(special_code)
+    fullname = fullname_filter(text)
 
-    if special_code == SPECIAL_CODE[update.effective_user.id]:
+    if fullname:
+
+        user_input_data['name'] = fullname[0]
+        user_input_data['surname'] = fullname[1]
 
         insert_user(user_input_data)
         set_user_data_in_bot_data(update.effective_user.id, bot_data)
 
         if user_input_data[LANG] == LANGS[0]:
-            text = 'Tabriklaymiz !\n\n' \
-                   'Siz registratsiyadan muvofaqqiyatli o\'tdingiz.'
+            text = 'Tabriklaymiz !\n' \
+                   'Siz registratsiyadan muvofaqqiyatli o\'tdingiz\n\n' \
+                   'E\'lon berishingiz mumkin'
 
         if user_input_data[LANG] == LANGS[1]:
-            text = 'Поздравляем !\n\n' \
-                   'Вы успешно зарегистрировались'
+            text = 'Поздравляем !\n' \
+                   'Вы успешно зарегистрировались\n\n' \
+                   'Вы можете помешать объявление'
 
         text = '\U0001F44F\U0001F44F\U0001F44F ' + text
-        reply_keyboard = ReplyKeyboard('menu_keyboard', user_input_data[LANG]).get_keyboard()
 
+        reply_keyboard = ReplyKeyboard('menu_keyboard', user_input_data[LANG]).get_keyboard()
         update.message.reply_text(text, reply_markup=reply_keyboard)
 
         user_input_data.clear()
@@ -227,31 +152,31 @@ def special_code_callback(update: Update, context: CallbackContext):
     else:
 
         if user_input_data[LANG] == LANGS[0]:
-            text = 'Maxsus kodni xato kiritdingiz !\n\n' \
-                   'Kodni qaytadan yuboring.'
+            text = 'Ism va familya xato yuborildi !\n' \
+                   'Qaytada quyidagi formatda yuboring'
+            example = 'Misol: Sherzod Esanov'
 
-        elif user_input_data[LANG] == LANGS[1]:
-            text = 'Вы ввели специальный код по ошибке !\n\n' \
-                   'Введите код еще раз.'
+        if user_input_data[LANG] == LANGS[1]:
+            text = 'Имя и фамилия введено неверное !\n' \
+                   'Отправьте еще раз в следующем формате'
+            example = 'Пример: Шерзод Эсанов'
 
-        text = '\U000026A0 ' + text
-        update.message.reply_text(text, quote=True)
+        example = f'<b><i><u>{example}</u></i></b>'
+        text = f'\U000026A0 {text}:\n\n {example}'
 
-        return CODE
+        update.message.reply_html(text, quote=True)
+
+        return FULLNAME
 
 
 conversation_handler = ConversationHandler(
     entry_points=[
-        CommandHandler('start', do_command, filters=~Filters.update.edited_message),
+        CommandHandler(['start', 'menu'], do_command, filters=~Filters.update.edited_message),
     ],
     states={
-        LANG: [CallbackQueryHandler(lang_callback)],
+        LANG: [CallbackQueryHandler(lang_callback), MessageHandler(Filters.text, lang_callback)],
 
-        FULL_NAME: [MessageHandler(Filters.text, full_name_callback)],
-
-        PHONE_NUMBER: [MessageHandler(Filters.contact | Filters.text, phone_number_callback)],
-
-        CODE: [MessageHandler(Filters.text, special_code_callback)],
+        FULLNAME: [MessageHandler(Filters.text, fullname_callback)],
     },
     fallbacks=[
         # CommandHandler('cancel', do_cancel)
