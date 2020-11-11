8a7f5c8531f01d0718b72899f0e05314ebf357d5
sherkhan7 <9669470@gmail.com>
2020-11-11 09:03:12 +0500

    minor changes in filters/filters.py

--- filters/filters.py
+++ filters/filters.py
@@ -1,37 +1,52 @@
-def full_name_filter(full_name):
-    full_name = full_name.strip()
-    full_name = full_name.split()
+def fullname_filter(fullname):
+    fullname = fullname.strip()
+    fullname = fullname.split()
 
-    if len(full_name) == 2:
-        if full_name[0].isalpha() and full_name[-1].isalpha():
-            return full_name
+    if len(fullname) == 2:
 
-    return False
+        if fullname[0].isalpha() and fullname[-1].isalpha():
+
+            fullname = fullname
+
+        else:
+            fullname = False
+
+    return fullname
 
 
 def special_code_filter(special_code):
-    if not special_code.isdigit():
-        return None
+    special_code = special_code.replace(' ', '')
 
-    return int(special_code)
+    if special_code.isdigit():
+        special_code = int(special_code)
+
+    else:
+        special_code = False
+
+    return special_code
 
 
 def phone_number_filter(phone_number):
-    phone_number = phone_number.replace(" ", "")
+    phone_number = phone_number.replace(' ', '')
 
     if phone_number.isdigit():
 
         if len(phone_number) == 9:
             phone_number = '+998' + phone_number
 
-            return phone_number
+        elif len(phone_number) == 12 and phone_number.startswith('998'):
+            phone_number = '+' + phone_number
 
-        elif len(phone_number) == 13:
+        else:
+            phone_number = False
 
-            return phone_number
-    elif phone_number.startswith('+998') and len(phone_number) == 13 and phone_number[1:].isdigit():
-        return phone_number
+    elif len(phone_number) == 13 and phone_number.startswith('+998') and phone_number[1:].isdigit():
 
-    return False
+        phone_number = phone_number
 
-# print(phone_number_filter('+998998559819'))
+    else:
+        phone_number = False
+
+    return phone_number
+
+# print(phone_number_filter('+99899333222'))
