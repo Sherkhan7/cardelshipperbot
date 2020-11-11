db0eba9658cd4c6c3d99fa0d0619a3f817fb4bf2
sherkhan7 <9669470@gmail.com>
2020-11-11 10:04:52 +0500

    major changes in messagehandler.py

--- handlers/messagehandler.py
+++ handlers/messagehandler.py
@@ -10,9 +10,9 @@ from pprint import pprint
 def message_handler_callback(update: Update, context: CallbackContext):
     # with open('jsons/update.json', 'w') as update_file:
     #     update_file.write(update.to_json())
+    # context.bot.edit_message_text('edited_text', -1001384173376, 315)
     full_text = update.message.text
     text = full_text.split(' ', 1)[-1]
-
     bot_data = context.bot_data
 
     # set bot_data[update.effective_user.id] -> dict
@@ -22,20 +22,9 @@ def message_handler_callback(update: Update, context: CallbackContext):
 
     if user:
 
-        if text == '/menu':
-
-            if user['lang'] == LANGS[0]:
-                reply_text = '\U0001F4D6 Menu'
-            if user['lang'] == LANGS[1]:
-                reply_text = '\U0001F4D6 Меню'
-
-            reply_keyboard = ReplyKeyboard('menu_keyboard', user['lang']).get_keyboard()
-            update.message.reply_text(reply_text, reply_markup=reply_keyboard)
-
-        elif text == 'Sozlamalar' or text == 'Настройки':
+        if text == 'Sozlamalar' or text == 'Настройки':
 
             reply_keyboard = ReplyKeyboard('settings_keyboard', user['lang']).get_keyboard()
-
             update.message.reply_text(full_text, reply_markup=reply_keyboard)
 
         elif text == 'Mening ma\'lumotlarim' or text == 'Мои данные':
@@ -48,7 +37,6 @@ def message_handler_callback(update: Update, context: CallbackContext):
             reply_text = 'Tilni tanlang.\nВыберите язык.'
 
             inline_keyboard = InlineKeyboard('langs_keyboard').get_keyboard()
-
             update.message.reply_text(reply_text, reply_markup=inline_keyboard)
 
         elif text == 'Ortga' or text == 'Назад':
@@ -58,16 +46,17 @@ def message_handler_callback(update: Update, context: CallbackContext):
 
         else:
 
-            reply_keyboard = ReplyKeyboard('menu_keyboard', user['lang']).get_keyboard()
-
             thinking_emoji = '\U0001F914'
 
+            reply_keyboard = ReplyKeyboard('menu_keyboard', user['lang']).get_keyboard()
             update.message.reply_text(thinking_emoji, quote=True, reply_markup=reply_keyboard)
 
     else:
 
-        update.message.reply_text('\U000026A0 Siz ro\'yxatdan o\'tmagansiz! \nBuning uchun /start ni bosing.\n\n'
-                                  '\U000026A0 Вы не зарегистрированы !\nДля этого нажмите /start.')
+        reply_text = '\U000026A0 Siz ro\'yxatdan o\'tmagansiz !\nBuning uchun /start ni bosing.\n\n' \
+                     '\U000026A0 Вы не зарегистрированы !\nДля этого нажмите /start.'
+
+        update.message.reply_text(reply_text)
 
 
-message_handler = MessageHandler(Filters.text, message_handler_callback)
+message_handler = MessageHandler(Filters.text & (~ Filters.command), message_handler_callback)
