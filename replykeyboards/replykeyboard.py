b604ab17dbf50dc54657cc46dfb7684649bd5655
sherkhan7 <9669470@gmail.com>
2020-11-13 11:32:23 +0500

    major changes: in replykeyboard.py, add new replykeyboardtypes.py

--- replykeyboards/replykeyboard.py
+++ replykeyboards/replykeyboard.py
@@ -1,85 +1,56 @@
 from telegram import ReplyKeyboardMarkup, KeyboardButton
-from languages import LANGS
+from replykeyboards.replykeyboardtypes import *
 
 
 class ReplyKeyboard(object):
 
-    def __init__(self, keyboard_type, lang=None):
+    def __init__(self, keyb_type, lang):
 
-        self.__type = keyboard_type
+        self.__type = keyb_type
         self.__lang = lang
 
         self.__keyboard = self.__create_reply_keyboard(self.__type, self.__lang)
 
     def __create_reply_keyboard(self, keyb_type, lang):
 
-        if keyb_type == 'phone_number_keyboard':
-            return self.__get_phone_number_keyboard(lang)
+        if keyb_type == keyboard_types[1]:
+            return self.__get_menu_keyboard(keyboard_types_lang[keyb_type][lang])
 
-        if keyb_type == 'menu_keyboard':
+        elif keyb_type == keyboard_types[2]:
 
-            return self.__get_menu_keyboard(lang)
+            return self.__get_settings_keyboard(keyboard_types_lang[keyb_type][lang])
 
-        elif keyb_type == 'settings_keyboard':
-
-            return self.__get_settings_keyboard(lang)
+        elif keyb_type == keyboard_types[3]:
+            return self.__get_phone_number_keyboard(keyboard_types_lang[keyb_type][lang])
 
     @staticmethod
     def __get_menu_keyboard(lang):
 
-        if lang == LANGS[0]:
-            button_1_text = 'Yuk e\'lon qilish'
-            button_2_text = 'Mashina e\'lon qilish'
-            button_3_text = 'Sozlamalar'
-            button_4_text = 'Mening e\'lonlarim'
-
-        if lang == LANGS[1]:
-            button_1_text = 'Объявить груз'
-            button_2_text = 'Объявить машину'
-            button_3_text = 'Настройки'
-            button_4_text = 'Мои объявления'
-
         return ReplyKeyboardMarkup([
 
-            [KeyboardButton(f'\U0001F4E6 {button_1_text}')],
-            [KeyboardButton(f'\U0001F4C4 {button_4_text}')],
-            [KeyboardButton(f'\U00002699 {button_3_text}')],
+            [KeyboardButton(f'\U0001F4E6 {lang[1]}')],
+            # [KeyboardButton(f'\U0001F4C4 {lang[2]}')],
+            [KeyboardButton(f'\U0001F4C4 {lang[3]}')],
+            [KeyboardButton(f'\U00002699 {lang[4]}')],
 
         ], resize_keyboard=True, one_time_keyboard=True)
 
     @staticmethod
     def __get_settings_keyboard(lang):
 
-        if lang == LANGS[0]:
-            button_1_text = 'Mening ma\'lumotlarim'
-            button_2_text = 'Tilni o\'zgartirish'
-            button_3_text = 'Ortga'
-
-        if lang == LANGS[1]:
-            button_1_text = 'Мои данные'
-            button_2_text = 'Изменить язык'
-            button_3_text = 'Назад'
-
         return ReplyKeyboardMarkup([
 
-            [KeyboardButton(f'\U0001F4D4 {button_1_text}'), ],
-            [KeyboardButton(f'\U0001F310 {button_2_text}'), ],
-            [KeyboardButton(f'\U00002B05 {button_3_text}'), ],
+            [KeyboardButton(f'\U0001F4D4 {lang[1]}')],
+            [KeyboardButton(f'\U0001F310 {lang[2]}')],
+            [KeyboardButton(f'\U00002B05 {lang[3]}')],
 
         ], resize_keyboard=True)
 
     @staticmethod
     def __get_phone_number_keyboard(lang):
-
-        if lang == LANGS[0]:
-            button_text = 'Kontaktimni yuborish'
-
-        if lang == LANGS[1]:
-            button_text = 'Отправить мой контакт'
-
         return ReplyKeyboardMarkup([
             [
-                KeyboardButton(f'\U0001F464 {button_text}', request_contact=True)]
+                KeyboardButton(f'\U0001F464 {lang[1]}', request_contact=True)]
         ], resize_keyboard=True)
 
     def get_keyboard(self):
---
+++ replykeyboards/replykeyboardtypes.py
@@ -0,0 +1,20 @@
+keyboard_types = {1: 'menu_keyboard', 2: 'settings_keyboard', 3: 'phone_number_keyboard'}
+
+keyboard_types_lang = {
+    "menu_keyboard": {
+        "uz": {1: "Yuk e'lon qilish", 2: "Mashina e'lon qilish", 3: "Mening e'lonlarim", 4: "Sozlamalar"},
+        "cy": {1: "Юк еълон қилиш", 2: "Машина еълон қилиш", 3: "Менинг еълонларим", 4: "Созламалар"},
+        "ru": {1: "Объявить груз", 2: "Объявить машину", 3: "Мои объявления", 4: "Настройки"}},
+
+    "settings_keyboard": {
+        "uz": {1: "Mening ma'lumotlarim", 2: "Tilni o'zgartirish", 3: "Ortga"},
+        "cy": {1: "Менинг маълумотларим", 2: "Тилни ўзгартириш", 3: "Ортга"},
+        "ru": {1: "Мои данные", 2: "Изменить язык", 3: "Назад"},
+    },
+
+    "phone_number_keyboard": {
+        "uz": {1: "Kontaktimni yuborish"},
+        "cy": {1: "Контактимни юбориш"},
+        "ru": {1: "Отправить мой контакт"}
+    }
+}
