723dd13e1862e34cdf1c1be05e23cdd639423be5
sherkhan7 <9669470@gmail.com>
2020-11-15 13:29:43 +0500

    minor ch: add new variables

--- globalvariables/globalvariables.py
+++ globalvariables/globalvariables.py
@@ -1,17 +1,17 @@
-USER_ID, CLIENT_NAME, CLIENT_SURNAME, CLIENT_USERNAME, FROM_REGION, FROM_DISTRICT, FROM_LOCATION = \
-    ('user_id', 'client_name', 'client_surname', 'client_username', 'from_region', 'from_district', 'from_location')
+USER_ID, TG_ID, USER_TG_ID, NAME, SURNAME, USERNAME, FROM_REGION, FROM_DISTRICT, FROM_LOCATION = \
+    ('user_id', 'tg_id', 'user_tg_id', 'name', 'surname', 'username', 'from_region', 'from_district', 'from_location')
 
 TO_REGION, TO_DISTRICT, TO_LOCATION, WEIGHT_UNIT, WEIGHT, VOLUME_UNIT = \
     ('to_region', 'to_district', 'to_location', 'weight_unit', 'weight', 'volume_unit')
 
-VOLUME, DEFINITION, PHOTO, DATE, TIME, CLIENT_PHONE_NUMBER, CONFIRMATION, EDIT = \
-    ('volume', 'definition', 'photo', 'date', 'time', 'client_phone_number', 'confirmation', 'edit')
+VOLUME, DEFINITION, PHOTO, DATE, TIME, USER_PHONE_NUMBER, CONFIRMATION, EDIT, MESSAGE_ID = \
+    ('volume', 'definition', 'photo', 'date', 'time', 'user_phone_number', 'confirmation', 'edit', 'message_id')
 
 FROM_TEXT, TO_TEXT, WEIGHT_TEXT, VOLUME_TEXT, DEFINITION_TEXT, DATE_TEXT = \
-    ("from_text", "to_text", "weight_text", "volume_text", "definition_text", "date_text")
+    ('from_text', 'to_text', 'weight_text', 'volume_text', 'definition_text', 'date_text')
 
-TIME_TEXT, CLIENT_TEXT, CLIENT_PHONE_NUMBER_TEXT, STATE, REGION_NAME = \
-    ("time_text", "client_text", "client_phone_number_text", "state", "region_name")
+TIME_TEXT, CLIENT_TEXT, CLIENT_PHONE_NUMBER_TEXT, STATE, REGION_NAME, ID, LANG = \
+    ('time_text', 'client_text', 'client_phone_number_text', 'state', 'region_name', 'id', 'lang')
 
 STATUS_TEXT, OPENED_STATUS, CLOSED_STATUS, NOT_CONFIRMED_STATUS, TG_ACCOUNT_TEXT, UNDEFINED_TEXT = \
-    ("status_text", "opened_status", "closed_status", "not_confirmed_status", "tg_account_text", "undifened_text")
+    ('status_text', 'opened_status', 'closed_status', 'not_confirmed_status', 'tg_account_text', 'undifened_text')
